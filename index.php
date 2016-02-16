59b2d990988010cf040a6e877bad5e3ad5db6e24
Tomasz Krzyżowski <t.krzyzowski96@gmail.com>
2016-02-16 16:52:28 +0100

    Make more comfy to use register and log in

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php 
+<?php
+
+/*
+--------------------------------------------------
+To do :
+verify user email and add it to database (done)
+send an email with activation link (done),
+Change Password functionality
+Update Name and Phone Number functionality
+Forgot username or password functionality
+--------------------------------------------
+*/
+
+
+
+
+
+
+
 require_once 'core/init.php';
 if (Session::exists('home')){
     echo Session::flash('home');
@@ -7,10 +25,15 @@ $user = new User();
 if ($user->isLoggedIn()){
     echo 'Logged in';
     ?>
-    
+
     <ul>
         <li><a href="logout.php">Log out!</a></li>
+        <li><a href="changepassword.php"></a></li>
     </ul>
-    
-<?php }
+
+<?php } else{
+?>
+  <p> You need to <a href="login.php">Log in</a> or <a href="register.php">Register!</a>
+<?php
+}
 ?>
